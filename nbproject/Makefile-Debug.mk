9f30f32b073e34c1a2a9a51591f88e48f4125184
Daedalus <daedalus.raistlin@gmail.com>
2019-08-14 12:49:55 +1000

    Add GCC Makefile
    Generated from Netbeans

---
+++ nbproject/Makefile-Debug.mk
@@ -0,0 +1,113 @@
+#
+# Generated Makefile - do not edit!
+#
+# Edit the Makefile in the project folder instead (../Makefile). Each target
+# has a -pre and a -post target defined where you can add customized code.
+#
+# This makefile implements configuration specific macros and targets.
+
+
+# Environment
+MKDIR=mkdir
+CP=cp
+GREP=grep
+NM=nm
+CCADMIN=CCadmin
+RANLIB=ranlib
+CC=gcc
+CCC=g++
+CXX=g++
+FC=gfortran
+AS=as
+
+# Macros
+CND_PLATFORM=Cygwin_x86_64-Windows
+CND_DLIB_EXT=dll
+CND_CONF=Debug
+CND_DISTDIR=dist
+CND_BUILDDIR=build
+
+# Include project Makefile
+include Makefile
+
+# Object Directory
+OBJECTDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}
+
+# Object Files
+OBJECTFILES= \
+	${OBJECTDIR}/ALisp.o \
+	${OBJECTDIR}/atoms.o \
+	${OBJECTDIR}/cell.o \
+	${OBJECTDIR}/core.o \
+	${OBJECTDIR}/eval_simple.o \
+	${OBJECTDIR}/parser.o
+
+
+# C Compiler Flags
+CFLAGS=
+
+# CC Compiler Flags
+CCFLAGS=
+CXXFLAGS=
+
+# Fortran Compiler Flags
+FFLAGS=
+
+# Assembler Flags
+ASFLAGS=
+
+# Link Libraries and Options
+LDLIBSOPTIONS=
+
+# Build Targets
+.build-conf: ${BUILD_SUBPROJECTS}
+	"${MAKE}"  -f nbproject/Makefile-${CND_CONF}.mk ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/alisp.exe
+
+${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/alisp.exe: ${OBJECTFILES}
+	${MKDIR} -p ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}
+	${LINK.cc} -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/alisp ${OBJECTFILES} ${LDLIBSOPTIONS}
+
+${OBJECTDIR}/ALisp.o: ALisp.cpp
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -std=c++14 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/ALisp.o ALisp.cpp
+
+${OBJECTDIR}/atoms.o: atoms.cpp
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -std=c++14 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/atoms.o atoms.cpp
+
+${OBJECTDIR}/cell.o: cell.cpp
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -std=c++14 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/cell.o cell.cpp
+
+${OBJECTDIR}/core.o: core.cpp
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -std=c++14 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/core.o core.cpp
+
+${OBJECTDIR}/eval_simple.o: eval_simple.cpp
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -std=c++14 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/eval_simple.o eval_simple.cpp
+
+${OBJECTDIR}/parser.o: parser.cpp
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -std=c++14 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/parser.o parser.cpp
+
+# Subprojects
+.build-subprojects:
+
+# Clean Targets
+.clean-conf: ${CLEAN_SUBPROJECTS}
+	${RM} -r ${CND_BUILDDIR}/${CND_CONF}
+
+# Subprojects
+.clean-subprojects:
+
+# Enable dependency checking
+.dep.inc: .depcheck-impl
+
+include .dep.inc
---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=Cygwin_x86_64-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin_x86_64-Windows
+CND_ARTIFACT_NAME_Debug=alisp
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin_x86_64-Windows/alisp
+CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin_x86_64-Windows/package
+CND_PACKAGE_NAME_Debug=alisp.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin_x86_64-Windows/package/alisp.tar
+# Release configuration
+CND_PLATFORM_Release=Cygwin_x86_64-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/Cygwin_x86_64-Windows
+CND_ARTIFACT_NAME_Release=alisp
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin_x86_64-Windows/alisp
+CND_PACKAGE_DIR_Release=dist/Release/Cygwin_x86_64-Windows/package
+CND_PACKAGE_NAME_Release=alisp.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin_x86_64-Windows/package/alisp.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk

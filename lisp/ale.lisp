12a78dde190bd8b9bc35ffaacf10b0aaf898f296
Daedalus <daedalus.raistlin@gmail.com>
2019-12-05 13:00:23 +1100

    First version of pattern-matching evaluator

--- lisp/ale.lisp
+++ lisp/ale.lisp
@@ -1,6 +1,5 @@
-;; Provides a feature complete evaluator that allows for better debugging.
-;; Can evaluate itself, very slowly. When evaluating itself, -s and -t are
-;; often used.
+;; The ALisp Experimental interpreter.
+;; Uses a Simple Pattern Matching algorithm to implement the evaluator.
 ;;
 ;; Usages:
 ;;   [alisp] teval.lisp [-s] [file to run.lisp]
@@ -34,25 +33,25 @@
 	;; Note that this macro takes a variable number of arguments.
 	(define or (fastmacro Conds
 		(if (empty? Conds)
-			false
+			'false
 			;; (if cond true (or rest...))
 			(list 'if (head Conds)
-				true
+				'true
 				(+ (list 'or) (tail Conds))))))
 
-	;; Evaluation depth. Recursive calls to seval increase this.
+	;; Evaluation depth. Recursive calls to aleval increase this.
 	(define Depth 0)
 	;; inverted flags so (not /flag/) is not needed
 	;; controlled by -s flag
 	(define NotSilent true) ;; whether to print (dbg) messages
 
 	;; Main evaluation function
-	(define seval (lambda (X Env) (begin
+	(define aleval (lambda (X Env) (begin
 		(set! Depth (+ 1 Depth))
 		(if NotSilent
-				(dbg "(seval  " (str-limit X) Env ")"))
+				(dbg "(aleval  " (str-limit X) Env ")"))
 		(define Result (do-next X Env))
-		(dbg "(seval  " (str-limit X) Env ") =>" (str-limit Result))
+		(dbg "(aleval  " (str-limit X) Env ") =>" (str-limit Result))
 		(set! Depth (- Depth 1))
 		Result
 	)))
@@ -82,23 +81,25 @@
 	(define depthness (lambda ()
 		(if (<= Depth 9) Depth "+")))
 
-	(define TypeSelector (env:new))
-	(env:define 'number (lambda (X Env) X) TypeSelector)
-	(env:define 'string (lambda (X Env) X) TypeSelector)
-	(env:define 'atom   (lambda (X Env) (env:get X Env)) TypeSelector)
+	;; Simple Pattern Matching algorithm lookup tables
+	(define Type#match (env:new nil))
+	(env:define 'number (lambda (X Env) X) Type#match)
+	(env:define 'string (lambda (X Env) X) Type#match)
+	(env:define 'atom   (lambda (X Env) (env:get X Env)) Type#match)
+	(define Type#_      (lambda (X Env)
+		(if (empty? X)
+			'nil
+			(next do-complex X Env))))
 
 	;; Perform the next step in evaluation. Essentially the same as calling
-	;; seval, but does not increase/decrease depth counter.
+	;; aleval, but does not increase/decrease depth counter.
 	;; -spec do-next(expr(), env()) -> any()
 	(define do-next (lambda (X Env) (begin
 		(if NotSilent
 			(dbg "(do-next" (str-limit X) Env))
-		(define T (typeof X))
-		(if (env:defined? T TypeSelector)
-			(next (env:get T TypeSelector) X Env)
-			(if (empty? X)
-				nil
-				(next do-complex X Env)))
+		;;(dbg "getor:" (env:getor (typeof X) '_ Type#match))
+		;; (env:dump Type#match)
+		(next (env:getor (typeof X) Type#_ Type#match) X Env)
 	)))
 
 	;; Perform the complex evaluation of the given expression.
@@ -109,15 +110,17 @@
 			(next do-proc (head X) (tail X) Env false))
 	))
 
-	(define Builtins (env:new))
+	(define Builtins (env:new nil))
+	(env:define 'quote     (lambda (X    ) (index X 1)))
 	(env:define 'next      (lambda (X Env) (next do-proc-next (tail X) Env)) Builtins)
 	(env:define 'if        (lambda (X Env) (next do-if (tail X) Env)) Builtins)
 	(env:define 'begin     (lambda (X Env) (next do-begin (tail X) Env)) Builtins)
-	(env:define 'define    (lambda (X Env) (env:define (index X 1) (seval (index X 2) Env) Env)) Builtins)
-	(env:define 'set!      (lambda (X Env) (env:set! (index X 1) (seval (index X 2) Env) Env)) Builtins)
+	(env:define 'define    (lambda (X Env) (env:define (index X 1) (aleval (index X 2) Env) Env)) Builtins)
+	(env:define 'set!      (lambda (X Env) (env:set! (index X 1) (aleval (index X 2) Env) Env)) Builtins)
 	(env:define 'lambda    (lambda (X Env) (cell:lambda (index X 1) (index X 2) Env)) Builtins)
 	(env:define 'macro     (lambda (X Env) (cell:macro (index X 1) (index X 2) Env)) Builtins)
 	(env:define 'fastmacro (lambda (X Env) (cell:fastmacro (index X 1) (index X 2) Env)) Builtins)
+	(define Builtins#_     (lambda (X Env) (next do-proc (head X) (tail X) Env false)))
 
 	;; If the first item in X is identified as an atom, check if it matches
 	;; a builtin. If not, do-proc is used an the expression treated as a function
@@ -125,11 +128,7 @@
 	(define do-builtin (lambda (X Env)
 		(next do-builtin-check X (head X) Env)))
 	(define do-builtin-check (lambda (X Xh Env)
-		(if (env:defined Xh Builtins)
-			(next (env:get Xh Builtins) X Env)
-			;; else
-			(next do-proc (head X) (tail X) Env false)
-		)
+		(next (env:getor Xh Builtins#_ Builtins) X Env)
 	))
 
 	;; Perform the builtin "(if Test Conseq Alt)".
@@ -140,7 +139,7 @@
 		(define Test (index Args 0))
 		(define Conseq (index Args 1))
 		(define Alt (index Args 2))
-		(define Result (seval Test Env))
+		(define Result (aleval Test Env))
 		(define Return (if (= false Result) Alt Conseq))
 		(if NotSilent
 			(dbg "if" (str-limit Test) (str-limit Conseq) (str-limit Alt) "=>" (str-limit Return)))
@@ -156,7 +155,7 @@
 			;; Hack: call do-begin with too many arguments, to avoid a begin
 			;; block. Element in Bodies will still be evaluated, just not stored
 			;; anywhere.
-			(next do-begin (tail Bodies) Env (seval (head Bodies) Env)))
+			(next do-begin (tail Bodies) Env (aleval (head Bodies) Env)))
 	))
 
 	;; Call a proc in the "next" mode, which means to reuse the current
@@ -165,10 +164,40 @@
 	(define do-proc-next (lambda (X Env)
 		(next do-proc (head X) (tail X) Env true)))
 
-	(define ProcExps (env:new))
+	(define ProcExps (env:new nil))
 	(env:define 'macro (lambda (Exps Env) Exps) ProcExps)
 	(env:define 'fastmacro (lambda (Exps Env) Exps) ProcExps)
-	(env:define '_ (lambda (Exps Env) (map Exps0 (lambda (E) (seval E Env0)))) ProcExps)
+	(define ProcExps#_ (lambda (Exps Env) (map Exps (lambda (E) (aleval E Env)))))
+
+	(define ProcDispatch (env:new nil))
+	(env:define 'lambda    (lambda (Exps Env0 Proc1 IsNext) (begin
+		;; (dbg "is lambda")
+		(define Env1
+			(if IsNext
+				(env:recapture Env0 (cell:lambda_args Proc1) Exps)
+				(env:capture (cell:lambda_args Proc1) Exps (cell:lambda_env Proc1))))
+		(next do-next (cell:lambda_body Proc1) Env1)
+	)) ProcDispatch)
+	(env:define 'macro     (lambda (Exps Env0 Proc1 IsNext) (begin
+		;; (dbg "is macro")
+		(define Env1
+			(if IsNext
+				(env:recapture Env0 (cell:lambda_args Proc1) Exps)
+				(env:capture (cell:lambda_args Proc1) Exps (cell:lambda_env Proc1))))
+		(define MR (aleval (cell:lambda_body Proc1) Env1))
+		(dbg "macro result:" MR)
+		(next do-next MR Env0)
+	)) ProcDispatch)
+	(env:define 'fastmacro (lambda (Exps Env0 Proc1 IsNext) (begin
+		(dbg "is fast macro")
+		(env:recapture (cell:lambda_env Proc1) (cell:lambda_args Proc1) Exps)
+		(define MR (aleval (cell:lambda_body Proc1) (cell:lambda_env Proc1)))
+		(dbg "fastmacro result:" MR)
+		(next do-next MR Env0)
+	)) ProcDispatch)
+	(env:define 'proc      (lambda (Exps Env0 Proc1 IsNext) (cell:proc Proc1 Exps)) ProcDispatch)
+	(env:define 'procenv   (lambda (Exps Env0 Proc1 IsNext) (cell:proc_env Proc1 Exps Env0)) ProcDispatch)
+	(define ProcDispatch#_ (lambda (Exps Env0 Proc1 IsNext) (error "Not an executable cell")))
 
 	;; Call a proc, be it lambda, macro, proc, or procenv.
 	;; Unless a macro, all arguments are first evaluated.
@@ -176,40 +205,13 @@
 	;; If a lambda or macro, tail recursion can be used here.
 	(define do-proc (lambda (Proc0 Exps0 Env0 IsNext) (begin
 		;; (dbg "(do-proc" Proc0 Exps0 Env0 ")")
-		(define Proc1 (seval Proc0 Env0))
+		(define Proc1 (aleval Proc0 Env0))
 		;; (dbg "(do-proc" Proc1 ")")
 		(define Pt (typeof Proc1))
 		(define Exps
-			((env:get (if (env:defined? Pt ProcExps) Pt '_) ProcExps) Exps0 Env))
+			((env:getor Pt ProcExps#_ ProcExps) Exps0 Env))
 		;; (dbg "exps" Exps)
-		(if (= 'lambda Pt) (begin
-			;; (dbg "is lambda")
-			(define Env1
-				(if IsNext
-					(env:recapture Env0 (cell:lambda_args Proc1) Exps)
-					(env:capture (cell:lambda_args Proc1) Exps (cell:lambda_env Proc1))))
-			(next do-next (cell:lambda_body Proc1) Env1)
-		) (if (= 'macro Pt) (begin
-			;; (dbg "is macro")
-			(define Env1
-				(if IsNext
-					(env:recapture Env0 (cell:lambda_args Proc1) Exps)
-					(env:capture (cell:lambda_args Proc1) Exps (cell:lambda_env Proc1))))
-			(define MR (seval (cell:lambda_body Proc1) Env1))
-			(dbg "macro result:" MR)
-			(next do-next MR Env0)
-		) (if (= 'fastmacro Pt) (begin
-			(dbg "is fast macro")
-			(env:recapture (cell:lambda_env Proc1) (cell:lambda_args Proc1) Exps)
-			(define MR (seval (cell:lambda_body Proc1) (cell:lambda_env Proc1)))
-			(dbg "fastmacro result:" MR)
-			(next do-next MR Env0)
-		) (if (= 'proc Pt)
-			(cell:proc Proc1 Exps)
-		(if (= 'proc_env Pt)
-			(cell:proc_env Proc1 Exps Env0)
-			(error "Not an executable cell")
-		)))))
+		((env:getor Pt ProcDispatch#_  ProcDispatch) Exps Env0 Proc1 IsNext)
 	)))
 
 	;; tail recursive factorial
@@ -255,7 +257,7 @@
 		))
 	))
 
-	(define Result (seval Code (env:new)))
+	(define Result (aleval Code (env:new)))
 	(print "Result:" Result)
 	;;(print "Argument state:")
 	;;(print " File:" File "NotSilent:" NotSilent "TailRecursive:" TailRecursive)

cd8f46ea42c8b7c3b707955e3c6ef2654916fc05
Daedalus <daedalus.raistlin@gmail.com>
2019-12-10 15:57:35 +1100

    Fix: str:repeat, env:new, env:dump, add env:getor
    Fixes:
    - str:repeat was not returning an ALVar
    - env:new can now be given 'nil as the parent, to create an environment
      with no parent
    - env:dump was broken for a long time
    
    Adds:
    - env:getor - get an item or return provided default value

--- lisp/pat2.lisp
+++ lisp/pat2.lisp
@@ -3,6 +3,9 @@
 ;; Erlang-style pattern matching.
 ;; Underscore '_ is a wildcard and matches anything.
 (begin
+	;; Convenience macros
+	(define defun (fastmacro (Name Arguments Body)
+		(list 'define Name (list 'lambda Arguments Body))))
 
 	;; Use Erlang-style list functions to build a dictionary.
 	;; A dictionary is just a list, with values in the form:
@@ -14,13 +17,13 @@
 	;; @param List      The list of items in the form: ( (Key Value) ... )
 	;; @param Comparer  The function to use to compare, or = for standard compare
 	;; @return          The full item found (ie, (Key Value)) or nil
-	(define lists:keyfind (lambda (Key KeyIndex List Comparer) (begin
+	(defun lists:keyfind (Key KeyIndex List Comparer) (begin
 		(if (not (env:defined 'Comparer))
 			(define Comparer =))
 		(if (empty? List)
-			nil
+			'nil
 			(next lists:keyfind/5 Key KeyIndex (head List) (tail List) Comparer))
-	)))
+	))
 
 	;; Find an item in a list using specified key and key index.
 	;; @param Key       The key to find
@@ -28,14 +31,14 @@
 	;; @param List      The list of items in the form: ( (Key Value) ... )
 	;; @param CompareKey The function to use to compare keys
 	;; @return          The full item found (ie, (Key Value)) or nil
-	(define lists:keyfind/5 (lambda (Key KeyIndex Item ListTail CompareKey)
+	(defun lists:keyfind/5 (Key KeyIndex Item ListTail CompareKey)
 		(if (CompareKey Key (index Item KeyIndex))
 			Item
 			(if (empty? ListTail)
-				nil
+				'nil
 				(next lists:keyfind/5 Key KeyIndex (head ListTail) (tail ListTail)))
 		)
-	))
+	)
 
 	;; Macro that implements logical and.
 	;; This logical and is short-circuited, that is if the first condition
@@ -43,13 +46,15 @@
 	;; (and [cond] [rest...]) ->
 	;;   (if [cond] (and [rest...]) false)
 	;; Note that this macro takes a variable number of arguments.
-	(define and (fastmacro conds (begin
-		(if (empty? conds)
-			true
+	;; Note that this macro does not recursively call itself,
+	;; rather it expands during each call exactly once.
+	(define and (fastmacro Conds (begin
+		(if (empty? Conds)
+			'true
 			;; (if cond (and rest...) false)
-			(list 'if (head conds)
-				(+ (list 'and) (tail conds))
-				false)))))
+			(list 'if (head Conds)
+				(+ (list 'and) (tail Conds))
+				'false)))))
 
 	;; Macro that implements logical or.
 	;; This logical or is short-circuited, that is if the first condition
@@ -57,28 +62,36 @@
 	;; (or [cond] [rest...]) ->
 	;;   (if [cond] true (or [rest...]))
 	;; Note that this macro takes a variable number of arguments.
+	;; Note that this macro does not recursively call itself,
+	;; rather it expands during each call exactly once.
 	(define or (fastmacro Conds
 		(if (empty? Conds)
-			false
+			'false
 			;; (if cond true (or rest...))
 			(list 'if (head Conds)
-				true
+				'true
 				(+ (list 'or) (tail Conds))))))
 
+	;; List Pattern Matching algorithm (LPMa)
 	(define pattern#matches (list
+		;; Two lists are compared for equality, recursively
+		;; calling (pattern...) on each item.
 		(list 'listlist (lambda (A B)
 			(if (and (empty? A) (empty? B))
 				'true
 				(if (pattern (head A) (head B))
 					(pattern (tail A) (tail B))
 					'false))))
+		;; Underscore and list always matches.
 		(list '_list    (lambda (A B) 'true))
+		;; List and underscore always matches.
 		(list 'list_    (lambda (A B) 'true))
+		;; For any other set, use standard equality operator.
 		(list '_        (lambda (A B) (= A B)))
 	))
-	(define under_or (lambda (Key) (if (= '_ Key) '_ (typeof Key))))
-	(define pattern-key (lambda (KeyA KeyB) (+ "" (under_or KeyA) (under_or KeyB))))
-	(define pattern (lambda (Key Item) (begin
+	(defun under_or (Key) (if (= '_ Key) '_ (typeof Key)))
+	(defun pattern-key (KeyA KeyB) (+ "" (under_or KeyA) (under_or KeyB)))
+	(defun pattern (Key Item) (begin
 		(define MatchKey (pattern-key Key Item))
 		;;(print "Match key:" MatchKey)
 		(define Result (lists:keyfind MatchKey 0 pattern#matches))
@@ -86,7 +99,7 @@
 			(set! Result (lists:keyfind '_ 0 pattern#matches)))
 		;;(print "Result:" (index Result 1))
 		((index Result 1) Key Item)
-	)))
+	))
 
 	(define Sample '(
 		("foo" "bar")

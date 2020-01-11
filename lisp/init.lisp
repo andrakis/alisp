b723fbc983df2a8611d94d1ca2c5137cc2a6e1f2
Daedalus <daedalus.raistlin@gmail.com>
2020-01-12 00:17:42 +1100

    Remove a few obsolete lisp files
    As well as abandoning the ALE interpreter in favour of the new anticipated
    virtual machine.

--- lisp/init.lisp
+++
@@ -1,6 +0,0 @@
-;; init.lisp - system startup and initialization
-(begin
-	(define main (lambda args (begin
-		(print "Arguments: " args)
-	)))
-)
--- lisp/test.lisp
+++
@@ -1,4 +0,0 @@
-(begin
-	(define foo (lambda x (print "You gave" x)))
-	(foo 1 2 3)
-)
